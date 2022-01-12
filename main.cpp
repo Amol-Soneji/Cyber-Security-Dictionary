95fff94042965e5cec9f841b07d78de56de0019b
Amol-Soneji <eon747@live.com>
2022-01-12 16:05:38 -0500

    Made more addition to main fipe code.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@
 using std::cout;
 using std::cin;
 
+Dictionary startDict(string param)
+{
+    if(param.compare(""));
+        return Dictionary dict();
+    else
+        return Dictionary dict(param);
+}
 
 int main()
 {
@@ -13,17 +20,15 @@ int main()
 		cout << "Enter 1 for standard cybersecurity dictionary, or enter 2 to use a custom dictionary.  :  \n";
 		cin >> dictTypeOption;
 	}
-	Dictionary dict();
+        string flName = "";
 	if (dictTypeOption == 2)
 	{
-		string flName = "";
 		while ((flName.compare("")) == 0)
 		{
 			cout << "Please enter a valid file-path or name of the dictionary file youwould like to create or open.  :  \n";
 			cin >> flName;
 		}
-		
-
 	}
 
+        new Dictionary dict = startDict(flName);
 }
